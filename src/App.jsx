4a6f15e94fdef82a028a6102407d64da0923194e
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-11-18 03:12:29 +0530

    Updated Structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Header} from "./Components/Header/Header";
+import {Footer} from "./Components/Footer/Footer";
+import {Main} from "./Components/Main/Main";
+import "./App.css";
+
+export default function App(){
+    return (
+        <>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </>
+    );
+};
